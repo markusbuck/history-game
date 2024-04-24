106fca0db441bd990c9e1539b4971d865e136695
KevinSotoMiranda <levinsoto5@gmail.com>
2024-04-24 15:04:24 -0600

    Added level3, level4, mainwindow, and model documentation

--- mainwindow.h
+++ mainwindow.h
@@ -9,6 +9,13 @@
 #include "player.h"
 #include "doorquestiondialog.h"
 #include "startleveldialogue.h"
+/*!
+ * Authors: Kevin Soto, Zak Holt, Walker Hall, Brenden Suess, and Markus Buckwalter
+ * CS 3505 Assignment 9: Educational App
+ * Date: 4/24/24
+ *
+ * Header file for the MainWindow class
+ */
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -21,23 +28,79 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
+    /*!
+     * \brief MainWindow - Constructor for MainWindow.
+     * \param m - Reference of the model.
+     * \param parent - Null pointer.
+     */
     MainWindow(Model& m, QWidget *parent = nullptr);
+
+    /*!
+     * \brief ~MainWindow - Decontructor for MainWindow.
+     */
     ~MainWindow();
 
 public slots:
+    /*!
+     * \brief onSceneRender - This slot will render the given scene.
+     * \param scene - The scene to render.
+     */
     void onSceneRender(QPixmap& scene);
+
+    /*!
+     * \brief keyPressEvent - This slot will change the players movement state on the key being pressed.
+     * \param event - The key being pressed.
+     */
     void keyPressEvent(QKeyEvent* event) override;
+
+    /*!
+     * \brief keyReleaseEvent - This slot will change the players movement state on the key being released.
+     * \param event - The key being released.
+     */
     void keyReleaseEvent(QKeyEvent* event) override;
 
 signals:
+    /*!
+     * \brief setPlayerMoveState - This will be emitted to set the players move state.
+     * \param state - The new player state.
+     * \param isDown - Boolean if the key is being held down.
+     */
     void setPlayerMoveState(Player::Movement state, bool isDown);
+
+    /*!
+     * \brief doorCollision - This will be emitted when the player has collided with a door.
+     */
     void doorCollision();
+
+    /*!
+     * \brief updateDimensions - This will be emitted when the user has changed the windows dimensions.
+     * \param width - The new width.
+     * \param height - The new height.
+     */
     void updateDimensions(int width, int height);
+
+    /*!
+     * \brief showDialogue - This will be emitted to show the question's dialog.
+     */
     void showDialogue();
 
 protected:
+    /*!
+     * \brief paintEvent - This method will draw the scenes pixmap.
+     * \param event - The QPaintEvent.
+     */
     void paintEvent(QPaintEvent* event) override;
+
+    /*!
+     * \brief resizeEvent - This method will resize the games pixmap.
+     * \param event - The QResizeEvent.
+     */
     void resizeEvent(QResizeEvent* event) override;
+
+    /*!
+     * \brief closeEvent - This method will close the doors dialog when the game has been closed.
+     * \param event - The QCloseEvent.
+     */
     void closeEvent(QCloseEvent *event) override;
 
 private:
--- model.h
+++ model.h
@@ -14,6 +14,13 @@
 #include <QElapsedTimer>
 #include "WorldState.h"
 #include "level.h"
+/*!
+ * Authors: Kevin Soto, Zak Holt, Walker Hall, Brenden Suess, and Markus Buckwalter
+ * CS 3505 Assignment 9: Educational App
+ * Date: 4/24/24
+ *
+ * Header file for the model class
+ */
 
 class Model : public QWidget
 {
@@ -34,30 +41,98 @@ private:
     qint64 lastFrameTime = 0;
     const qint64 FRAME_TIME_TARGET = 1e9 / 60;
 
+    /*!
+     * \brief renderScene - This method will render the current level, and player to the scene.
+     */
     void renderScene();
 
     b2Vec2 windowDimensions;
 
+    /*!
+     * \brief setupLevel - This method will update the doors dialog to the current level's question text, and responses.
+     * \param level - The level to be updated.
+     */
     void setupLevel(Level *level);
 
 public:
+    /*!
+     * \brief Model - Constructor for Model.
+     * \param parent - Null pointer.
+     */
     Model(QWidget *parent = nullptr);
-    ~Model();
 
+    /*!
+     * \brief ~Model - Deconstructor for Model.
+     */
+    ~Model();
 signals:
+    /*!
+     * \brief renderSceneOnView - This will be emitted to render the current scene, and display it.
+     * \param scene - The scene to render.
+     */
     void renderSceneOnView(QPixmap& scene);
+
+    /*!
+     * \brief generateQuestionnaire - This will be emitted to generate the doors
+     * questionnaire with the given question text, and responses.
+     * \param questionText - The text to update.
+     * \param responses - The responses to update.
+     */
     void generateQuestionnaire(QString questionText, QHash<QString, bool> responses);
+
+    /*!
+     * \brief displayPopUp - This will be emitted to display the popup on a chosen response.
+     * \param input - A boolean to determine which type of popup to display.
+     * \param answer - The hint to display on the popup.
+     */
     void displayPopUp(bool input, QString answer);
+
+    /*!
+     * \brief sendCurrentContext - This will be emitted to change the contexts information.
+     * \param context - The updated information to be displayed.
+     */
     void sendCurrentContext(QString context);
+
+    /*!
+     * \brief showContextDialogue - This will be emitted to display the context dialog.
+     */
     void showContextDialogue();
     //void showInitialContextDialogue();
 
 public slots:
+    /*!
+     * \brief worldStep - This slot will update the scene.
+     */
     void worldStep();
+
+    /*!
+     * \brief onPlayerMoveState - This slot will change the players movement.
+     * \param state - The state to be changed to.
+     * \param isDown - Boolean to determine if the key is down.
+     */
     void onPlayerMoveState(Player::Movement state, bool isDown);
+
+    /*!
+     * \brief isInputCorrect - This slot will determine if the players input was correct on the doors dialog.
+     * \param response - The response the user has pressed.
+     */
     void isInputCorrect(QString response);
+
+    /*!
+     * \brief exitDialog - This slot will close the doors dialog when closed.
+     */
     void exitDialog();
+
+    /*!
+     * \brief updateDimensions - This slot will update the games window size.
+     * \param width - The width of the window.
+     * \param height - The height of the window.
+     */
     void updateDimensions(int width, int height);
+
+    /*!
+     * \brief getCurrentContext - This slot will get the context of the current level.
+     */
     void getCurrentContext();
 
 };
