388aa611679c6f3fbe2a3fcdc1debe3dd0119bf2
KevinSotoMiranda <levinsoto5@gmail.com>
2024-04-24 15:46:55 -0600

    Added documentation to contactlistener, level, level1, and level2

--- contactlistener.h
+++ contactlistener.h
@@ -4,17 +4,46 @@
 #include <Box2D/Box2D.h>
 #include <functional>
 #include <map>
-
+/*!
+ * Authors: Kevin Soto, Zak Holt, Walker Hall, Brenden Suess, and Markus Buckwalter
+ * CS 3505 Assignment 9: Educational App
+ * Date: 4/24/24
+ *
+ * Header file for the ContactListener.
+ */
 class ContactListener : public b2ContactListener
 {
 public:
+    /*!
+     * \brief ContactListener - Constructor for ContactListener.
+     */
     ContactListener();
 
     using Callback = std::function<void(bool began, b2Fixture*, b2Fixture*)>;
+
+    /*!
+     * \brief BeginContact - This method will add a callback to the object being hit.
+     * \param contact - The object being in contact.
+     */
     void BeginContact(b2Contact* contact) override;
+
+    /*!
+     * \brief EndContact - This method will remove a callback on the contact.
+     * \param contact - The object leaving contact.
+     */
     void EndContact(b2Contact* contact) override;
 
+    /*!
+     * \brief addCallback - This method will add a callback to the hashtable of callbacks.
+     * \param body - The key of the body.
+     * \param callback - The value of the callback.
+     */
     void addCallback(b2Body* body, Callback callback);
+
+    /*!
+     * \brief removeCallback - This method will remove a callback on the hashtable of callbacks.
+     * \param body - The key to remove.
+     */
     void removeCallback(b2Body* body);
 
 private:
--- level1.h
+++ level1.h
@@ -2,11 +2,26 @@
 #define LEVEL1_H
 
 #include "level.h"
-
+/*!
+ * Authors: Kevin Soto, Zak Holt, Walker Hall, Brenden Suess, and Markus Buckwalter
+ * CS 3505 Assignment 9: Educational App
+ * Date: 4/24/24
+ *
+ * Header file for the Level1 class
+ */
 class Level1 : public Level
 {
 public:
+    /*!
+     * \brief Level1 - Constructor for Level1.
+     * \param background - The file name of the background.
+     */
     Level1(QString background);
+
+    /*!
+     * \brief renderCustom - This method will render the level.
+     * \param painter - The painter to draw.
+     */
     void renderCustom(QPainter *painter) override;
 };
 
